7a43767a050b82ef440c6ea9220e32b6f330172d
Dominic Jack <thedomjack@gmail.com>
2018-04-11 09:46:51 +1000

    added bulk archive creation

--- core/voxels/scripts/create_archive.py
+++ core/voxels/scripts/create_archive.py
@@ -1,6 +1,7 @@
 
 
 def create_archive(cat_desc, voxel_dim, example_ids=None, overwrite=False):
+    import os
     import zipfile
     from shapenet.core.voxels.config import VoxelConfig
     from shapenet.core import get_example_ids, cat_desc_to_id
@@ -17,17 +18,33 @@ def create_archive(cat_desc, voxel_dim, example_ids=None, overwrite=False):
             if not overwrite and dst in namelist:
                 continue
             src = config.get_binvox_path(cat_id, example_id)
-            zf.write(src, dst)
+            if os.path.isfile(src):
+                zf.write(src, dst)
+            else:
+                print('No file at %s for %s/%s: skipping' %
+                      (src, cat_id, example_id))
+
+
+def create_all_archives(voxel_dim, overwrite=False):
+    from shapenet.core import get_cat_ids, cat_id_to_desc
+    cat_ids = get_cat_ids()
+    for cat_id in cat_ids:
+        cat_desc = cat_id_to_desc(cat_id)
+        print('Creating archive: %s' % cat_desc)
+        create_archive(cat_desc, voxel_dim, overwrite=overwrite)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('cat', type=str)
+    parser.add_argument('cat', type=str, nargs='?')
     parser.add_argument('-o', '--overwrite', action='store_true')
     parser.add_argument('-d', '--voxel_dim', type=int, default=32)
     parser.add_argument('-i', '--example_ids', nargs='*')
 
     args = parser.parse_args()
-    create_archive(
-        args.cat, args.voxel_dim, args.example_ids, args.overwrite)
+    if args.cat is None:
+        create_all_archives(args.voxel_dim, overwrite=args.overwrite)
+    else:
+        create_archive(
+            args.cat, args.voxel_dim, args.example_ids, args.overwrite)
